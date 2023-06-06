5fdc0ad8eef6511851f1e4ebeee0afb53ed2ff46
MadishettiPrashanth <prashanthmadishetti1999@gmail.com>
2023-06-06 15:29:18 +0530

    Happyhomes

---
+++ Student.java
@@ -0,0 +1,17 @@
+package com.slokam;
+
+public class Student {
+	int sage=23;
+	String sname="Pavan";
+	float ssalary=25.6f;
+	
+
+	public static void main(String[] args) {
+		Student std=new Student();
+		System.out.println(std.sage);
+		System.out.println(std.sname);
+		System.out.println(std.ssalary);
+
+	}
+
+}
